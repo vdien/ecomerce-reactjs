6b83afcd35d3f975d1f868c4a768a50f6bed9b17
LNVDien <lnvdien@gmail.com>
2022-12-15 23:04:02 +0700

    Layout, Home page,redux(Feature Product)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect } from "react";
+import { BrowserRouter } from "react-router-dom";
+import "./App.css";
+import Home from "./component/Home/Home";
+import WebFont from "webfontloader";
 function App() {
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"],
+      },
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Home />
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/actions/ProductActions.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+import {
+    ALL_PRODUCT_FAIL,
+    ALL_PRODUCT_REQUEST,
+    ALL_PRODUCT_SUCCESS,
+    CLEAR_ERRORS,
+} from "../constans/ProductConstans";
+
+export const getProduct = () => async(dispatch) => {
+    try {
+        dispatch({
+            type: ALL_PRODUCT_REQUEST,
+        });
+        let link = `/api/v2/products`;
+        const { data } = await axios.get(link);
+        dispatch({
+            type: ALL_PRODUCT_SUCCESS,
+            payload: data,
+        });
+    } catch (error) {
+        dispatch({
+            type: ALL_PRODUCT_FAIL,
+            payload: error.response.data.message,
+        });
+    }
+};
+
+//   Clearing errors
+export const clearErrors = () => async(dispatch) => {
+    dispatch({
+        type: CLEAR_ERRORS,
+    });
+};
