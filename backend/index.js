69097a2643115814d945562bcae793b0698d2051
LNVDien <lnvdien@gmail.com>
2022-12-29 21:43:10 +0700

    tét

--- backend/index.js
+++ backend/index.js
@@ -20,9 +20,8 @@ cloudinary.config({
 //create server
 
 const server = app.listen(process.env.PORT, () => {
-    console.log(`Server is working on http://localhost:${process.env.PORT}`);
+    console.log(`Server is working on ${process.env.PORT}`);
 });
-// Page Home
 
 // Unhandled promise rejection
 
